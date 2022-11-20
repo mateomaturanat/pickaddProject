a1fabc93c95edaa7aa1940f914089942629b23bc
HaroId <harolewu@gmail.com>
2022-11-19 20:48:39 -0500

    Functionality

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -34,12 +34,12 @@
             {{-- Menu/Logo --}}
             <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 {{-- Logotipo --}}
-                <div class="flex flex-shrink-0 items-center mx-10">
+                <a href="fields" class="flex flex-shrink-0 items-center mx-10">
                     <img class="block h-8 w-auto lg:hidden" src="{{ asset('Reservación_Canchas/Icons/LogoP.png') }}"
                         alt="Your Company">
                     <img class="hidden h-8 w-auto lg:block" src="{{ asset('Reservación_Canchas/Icons/LogoP.png') }}"
                         alt="Your Company">
-                </div>
+                </a>
 
                 {{-- Menu --}}
                 <div class="hidden sm:ml-6 sm:block">
@@ -63,56 +63,74 @@
                 </div>
             </div>
 
-            {{-- Notificacion/Profile Button --}}
-            <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                {{-- Notification button --}}
-                <button type="button"
-                    class="rounded-full bg-gray-300 p-1 text-gray-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-400">
-                    <span class="sr-only">View notifications</span>
-                    <!-- Heroicon name: outline/bell -->
-                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                        stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-                    </svg>
-                </button>
 
-                <!-- Profile dropdown -->
-                <div class="relative ml-3" x-data="{ open: false }">
-                    {{-- Img --}}
-                    <div>
-                        <button x-on:click="open = true" type="button"
-                            class="flex rounded-full bg-gray-400 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-400"
-                            id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                            <span class="sr-only">Open user menu</span>
-                            <img class="h-8 w-8 rounded-full"
-                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                alt="">
-                        </button>
-                    </div>
-                    <!--
-                    Dropdown menu, show/hide based on menu state.
-                    Entering: "transition ease-out duration-100"
-                    From: "transform opacity-0 scale-95"
-                    To: "transform opacity-100 scale-100"
-                    Leaving: "transition ease-in duration-75"
-                    From: "transform opacity-100 scale-100"
-                    To: "transform opacity-0 scale-95"
-                    -->
-                    {{-- Settings --}}
-                    <div x-show="open" x-on:click.away="open = false"
-                        class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                        role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-                        <!-- Active: "bg-gray-100", Not Active: "" -->
-                        <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
-                            id="user-menu-item-0">Your Profile</a>
-                        <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
-                            id="user-menu-item-1">Settings</a>
-                        <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
-                            id="user-menu-item-2">Sign out</a>
+            @auth
+                {{-- Notificacion/Profile Button --}}
+                <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                    {{-- Notification button --}}
+                    <button type="button"
+                        class="rounded-full bg-gray-300 p-1 text-gray-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-400">
+                        <span class="sr-only">View notifications</span>
+                        <!-- Heroicon name: outline/bell -->
+                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+                        </svg>
+                    </button>
+
+                    <!-- Profile dropdown -->
+                    <div class="relative ml-3" x-data="{ open: false }">
+                        {{-- Img --}}
+                        <div>
+                            <button x-on:click="open = true" type="button"
+                                class="flex rounded-full bg-gray-400 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-400"
+                                id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                                <span class="sr-only">Open user menu</span>
+                                <img class="h-8 w-8 rounded-full"
+                                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                    alt="">
+                            </button>
+                        </div>
+
+                        <!--Dropdown menu, show/hide based on menu state.
+                                                    Entering: "transition ease-out duration-100"
+                                                    From: "transform opacity-0 scale-95"
+                                                    To: "transform opacity-100 scale-100"
+                                                    Leaving: "transition ease-in duration-75"
+                                                    From: "transform opacity-100 scale-100"
+                                                    To: "transform opacity-0 scale-95"
+                                                    -->
+                        {{-- Settings --}}
+                        <div x-show="open" x-on:click.away="open = false"
+                            class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                            role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+                            <!-- Active: "bg-gray-100", Not Active: "" -->
+                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
+                                id="user-menu-item-0">Your Profile</a>
+                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
+                                id="user-menu-item-1">Settings</a>
+
+                            <form method="POST" action="{{ route('logout') }}" x-data>
+                                @csrf
+                                <a href="{{ route('logout') }}" class="block px-4 py-2 text-sm text-gray-700 "
+                                    role="menuitem" tabindex="-1" id="user-menu-item-2"
+                                    @click.prevent="$root.submit();">
+                                    Sign out
+                                </a>
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </div>
+            @else
+                <div>
+                    <a href="#"
+                        class="border hover:border-green-500 bg-transparent text-gray-800 px-3 py-2 rounded-md text-sm font-medium">Login</a>
+                    <a href="#"
+                        class="border hover:border-green-500 bg-transparent text-gray-800 px-3 py-2 rounded-md text-sm font-medium">Register</a>
+                </div>
+            @endauth
+
         </div>
     </div>
 
